8bb82e456044dc57d2c046b1450c47bcad8fb9fb
batistapedro <batistapedro271188@gmail.com>
2018-11-16 16:15:47 -0400

    agreando varios metodo para poder editar y crear servicios

--- src/app/admin/servicios/form-servicio/form-servicio.component.ts
+++ src/app/admin/servicios/form-servicio/form-servicio.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Servicio } from 'src/app/models/servicio';
+import { servicioUnico } from 'src/app/validador/helper';
+import { ServiciosService } from 'src/app/services/servicios.service';
 
 @Component({
   selector: 'app-form-servicio',
@@ -16,34 +18,60 @@ export class FormServiciosComponent implements OnInit {
   servicio: Servicio;
   constructor(
     private _fb: FormBuilder,
+    public _serv: ServiciosService,
     public dialogRef: MatDialogRef<FormServiciosComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Servicio
     ) { }
 
   ngOnInit() {
-    this.initForm();
 
+    this.init();
+
+  }
+
+  init() {
+    if (this.data) {
+      if (this.data.id) {
+        this.servicio = this.data;
+        delete this.data;
+        this.editar();
+      }
+    } else {
+
+      this.initForm();
+    }
   }
 
   initForm() {
     this.formServicio = this._fb.group({
       id: '',
-      nombre: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(5), Validators.pattern('[a-zA-Z ]*')]],
+      nombre: ['',
+        {
+          validators:
+          [
+            Validators.required,
+            Validators.maxLength(30),
+            Validators.minLength(5),
+            Validators.pattern('[a-zA-Z\u00f1\u00d1 ]*')
+          ],
+          asyncValidators : [servicioUnico(this._serv).bind(this)],
+          updateOn: 'blur'
+        }
+      ],
       estado: ['', [Validators.required, Validators.pattern('[0-1]{1}')]]
     });
-    // comprobamos si existe el objecto data para la edicion
-    if (this.data) {
-      // comprobamos si el objecto data tiene un id
-      if (this.data.id) {
-        this.servicio = this.data;
-        delete this.data;
-        // llamamo a la funcion editar
-        this.editar();
-      }
-    }
+  }
+
+  initFormEdit() {
+    this.formServicio = this._fb.group({
+      id: '',
+      nombre: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(5), Validators.pattern('[a-zA-Z\u00f1\u00d1 ]*')]],
+      estado: ['', [Validators.required, Validators.pattern('[0-1]{1}')]]
+    });
   }
 
   editar() {
+      this.initFormEdit();
       this.formServicio.setValue({
         id: this.servicio.id,
         nombre : this.servicio.nombre,
