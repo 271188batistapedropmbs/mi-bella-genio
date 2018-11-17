ef96868c9bd8460d182dda282ed05f7e447e4a94
batistapedro <batistapedro271188@gmail.com>
2018-11-17 13:08:59 -0400

    mejorando los metodos y vista del formulario banco

--- src/app/admin/bancos/form-bancos/form-bancos.component.ts
+++ src/app/admin/bancos/form-bancos/form-bancos.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { Banco } from 'src/app/models/banco';
 
 @Component({
   selector: 'app-form-bancos',
@@ -9,21 +11,127 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FormBancosComponent implements OnInit {
 
   formBanco: FormGroup;
-  constructor(private _fb: FormBuilder) { }
+  banco: Banco;
+  estado: Array<number> = [0, 1];
+  constructor(
+    private _fb: FormBuilder,
+    public dialogRef: MatDialogRef<FormBancosComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Banco
+    ) { }
 
   ngOnInit() {
-    this.initForm();
+    this.init();
+  }
+
+  init() {
+    if (this.data) {
+      if (this.data.id) {
+        this.banco = this.data;
+        delete this.data;
+        this.editar();
+      }
+    } else {
+
+      this.initForm();
+    }
   }
 
   initForm() {
     this.formBanco = this._fb.group({
-      nombre: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(70), Validators.pattern('[a-zA-Z ]')]],
-      numero_cuenta: ['', [Validators.required, Validators.minLength(20), Validators.maxLength(20), Validators.pattern('[0-9]')]],
-      tipo_cuenta: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(6), Validators.pattern('[a-zA-Z]')]],
-      usuario_cuenta: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(80), Validators.pattern('[a-zA-Z ] *')]],
-      usuario_cedula: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(8), Validators.pattern('[0-9]')]],
+      id: '',
+      nombre: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(70), Validators.pattern('[a-zA-Z ]*')]],
+      numero_cuenta: ['', [Validators.required, Validators.minLength(20), Validators.maxLength(20), Validators.pattern('[0-9]*')]],
+      tipo_cuenta: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(6), Validators.pattern('[a-zA-Z]*')]],
+      usuario_cuenta: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(80), Validators.pattern('[a-zA-Z ]*')]],
+      usuario_cedula: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(8), Validators.pattern('[0-9]*')]],
       estado: ['', [Validators.required, Validators.pattern('[0-1]{1}')]]
     });
+  }
+
+  initFormEdit() {
+    this.formBanco = this._fb.group({
+      id: this.banco.id,
+      nombre:
+      [
+        this.banco.nombre,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(70),
+          Validators.pattern('[a-zA-Z ]*')
+        ]
+      ],
+      numero_cuenta:
+      [
+        this.banco.numero_cuenta,
+        [
+          Validators.required,
+          Validators.minLength(20),
+          Validators.maxLength(20),
+          Validators.pattern('[0-9]*')
+        ]
+      ],
+      tipo_cuenta:
+      [
+        this.banco.tipo_cuenta,
+        [
+          Validators.required,
+          Validators.maxLength(10),
+          Validators.minLength(6),
+          Validators.pattern('[a-zA-Z]*')
+        ]
+      ],
+      usuario_cuenta:
+      [
+        this.banco.usuario_cuenta,
+        [
+          Validators.required,
+          Validators.minLength(7),
+          Validators.maxLength(80),
+          Validators.pattern('[a-zA-Z ]*')
+        ]
+      ],
+      usuario_cedula:
+      [
+        this.banco.usuario_cedula,
+        [
+          Validators.required,
+          Validators.minLength(7),
+          Validators.maxLength(8),
+          Validators.pattern('[0-9]*')
+        ]
+      ],
+      estado:
+      [
+        this.banco.estado,
+        [
+          Validators.required,
+          Validators.pattern('[0-1]{1}')
+        ]
+      ]
+    });
+  }
+
+  editar() {
+    this.initFormEdit();
+    /*this.formBanco.setValue({
+      id : this.banco.id,
+      nombre: this.banco.nombre,
+      numero_cuenta: this.banco.numero_cuenta,
+      tipo_cuenta : this.banco.tipo_cuenta,
+      usuario_cuenta: this.banco.usuario_cuenta,
+      usuario_cedula: this.banco.usuario_cedula,
+      estado: this.banco.estado
+    });*/
+  }
+
+  onSubmit() {
+    if (!this.formBanco.valid) {
+      return false;
+    } else {
+      // paso datos al component servicios y cierro el form dialogo
+      this.dialogRef.close(this.formBanco.value);
+    }
   }
 
 
