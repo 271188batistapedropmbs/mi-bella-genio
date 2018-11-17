6fa2c94519d679ea371d30a9c7dcda299fc8c736
batistapedro <batistapedro271188@gmail.com>
2018-11-17 13:08:12 -0400

    agregando funcionalidad del crud de bancos

--- src/app/admin/bancos/bancos.component.ts
+++ src/app/admin/bancos/bancos.component.ts
@@ -15,7 +15,7 @@ export class BancosComponent implements OnInit, OnDestroy {
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  // bbancos: Banco[] = [];
+  bancos: Banco[] = [];
   subscription: Subscription;
   dataSource: any;
   displayedColumns: string[] = ['#', 'nombre', 'numero_cuenta', 'tipo_cuenta', 'usuario_cuenta', 'usuario_cedula', 'estado', 'eliminar'];
@@ -24,8 +24,8 @@ export class BancosComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
    this.subscription =  this.servBanco.getBancos().subscribe( (resp: any) => {
-     // this.bancos = resp.bancos;
-      this.dataSource = new MatTableDataSource(resp.bancos);
+     this.bancos = resp.bancos;
+      this.dataSource = new MatTableDataSource(this.bancos);
 
       // ordenan dato de la table
       this.dataSource.sort = this.sort;
@@ -40,28 +40,108 @@ export class BancosComponent implements OnInit, OnDestroy {
     this.dataSource.filter = filtro.trim().toLocaleLowerCase();
   }
 
-  registrar() {
+
+  openDialogo(tipo: string, banco?: Banco): any {
 
     const dialogoConfig = new MatDialogConfig();
-    dialogoConfig.width = '50%';
+    dialogoConfig.width = '60%';
     dialogoConfig.height = 'auto';
 
-    this.dialogo.open(FormBancosComponent, dialogoConfig);
+    if (tipo === 'editar') {
+      dialogoConfig.data = banco;
+    }
 
+    const dialogoRef = this.dialogo.open(FormBancosComponent, dialogoConfig);
+    return dialogoRef;
   }
 
-  eliminar(id) {
-    console.log(id);
+  registrar() {
+
+      const referencia = this.openDialogo('registrar');
+      referencia.afterClosed().subscribe(result => {
+        if (result) {
+          this.servBanco.registrar(result).subscribe(resp => {
+            this.snackbar.open(resp.respuesta, 'Ok', {
+              duration: 5000
+            });
+            result.id = resp.id;
+            this.bancos.push(result);
+            this.dataSource =  new MatTableDataSource(this.bancos);
+          },
+          error => {
+            console.log(error);
+          });
+        }
+      });
   }
 
-  editar(id) {
-    this.snackbar.open('prueba', 'esto es una prueba', {
-      duration: 3000,
+  editar(banco: Banco) {
+    const referencia = this.openDialogo('editar', banco);
+      referencia.afterClosed().subscribe(result => {
+        if (result) {
+          this.servBanco.editar(result).subscribe(resp => {
+            this.snackbar.open(resp.respuesta, 'Ok', {
+              duration: 5000
+            });
+
+            const index = this.bancos.indexOf(banco);
+            banco = result;
+            this.bancos.splice(index, 1, banco);
+            this.dataSource =  new MatTableDataSource(this.bancos);
+          },
+          error => {
+            console.log(error);
+          });
+        }
+      });
+  }
+
+  cambiarEstado(banco: Banco) {
+
+    let estado: number;
+
+    if (banco.estado) {
+      estado = 0;
+    } else {
+      estado = 1;
+    }
+    const newBanco: Banco = new Banco();
+
+    newBanco.id = banco.id;
+    newBanco.nombre = banco.nombre;
+    newBanco.numero_cuenta = banco.numero_cuenta;
+    newBanco.tipo_cuenta = banco.tipo_cuenta;
+    newBanco.usuario_cuenta = banco.usuario_cuenta;
+    newBanco.usuario_cedula = banco.usuario_cedula;
+    newBanco.estado = estado;
+
+    this.servBanco.editar(newBanco).subscribe(resp => {
+
+      this.snackbar.open(resp.respuesta, 'Ok', {
+        duration: 5000
+      });
+      banco.estado = estado;
     });
+    return false;
   }
 
-  cambiarEstado(element) {
-    console.log(element);
+  eliminar(banco: Banco) {
+    const confirmar = confirm('Esta seguro que deseas eliminar esto ? ');
+    if (!confirmar) {
+      return false;
+    }
+
+   this.servBanco.eliminar(banco.id).subscribe(resp => {
+      this.snackbar.open(resp.respuesta, 'Ok', {
+        duration: 5000
+      });
+    const index = this.bancos.indexOf(banco);
+    this.bancos.splice(index, 1);
+    this.dataSource = new MatTableDataSource(this.bancos);
+    },
+    error => {
+      console.log(error);
+    });
   }
 
   ngOnDestroy() {
