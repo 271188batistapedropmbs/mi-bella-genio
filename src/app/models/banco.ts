b8eedbd2baebfc8b77a9e7faffdaf83cd68de68a
batistapedro <batistapedro271188@gmail.com>
2018-11-17 13:03:05 -0400

    editando propiedades

--- src/app/models/banco.ts
+++ src/app/models/banco.ts
@@ -2,9 +2,9 @@ export class Banco {
 
     id?: number;
     nombre: string;
-    cuenta: string;
-    tipo: string;
-    nombre_titular: string;
-    cedula_titular: string;
+    numero_cuenta: string;
+    tipo_cuenta: string;
+    usuario_cuenta: string;
+    usuario_cedula: string;
     estado: number;
 }
